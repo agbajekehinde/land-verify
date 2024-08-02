8b33712bb5dde268d01c0195914daf6f0a81ce23
kenny94developer <hussenagbaje@gmail.com>
2024-08-02 12:23:30 +0100

    Seeded database

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Page() {
 <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to FinProduct.</strong> This is a product from{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Parent Fintech Company
             </a>
-            , brought to you by Vercel.
+            , the best FinAPI in the world.
           </p>
           <Link
             href="/login"
