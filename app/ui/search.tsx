3658e9fa3497ff294b8cec039cd1e981992463ee
kenny94developer <hussenagbaje@gmail.com>
2024-08-11 14:01:09 +0100

    invoices-pagination-update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
-
     const params = new URLSearchParams(searchParams);
+    console.log(`Searching... ${term}`);
+    
+    params.set('page', '1');
 
     if (term) {
       params.set('query', term);
