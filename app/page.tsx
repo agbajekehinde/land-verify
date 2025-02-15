e5ea3171e2e15a4447d3fb42fce0d3bb6efeebef
Kenny-Ibile <hussenagbaje@gmail.com>
2025-02-15 18:17:08 +0000

    Hero copy update

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
 <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
-            <strong>Welcome to LandVerify.</strong> This is a product from{' '}
+            <strong>Welcome to LandVerify NG.</strong> This is a product from{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Parent Fintech Company
             </a>
