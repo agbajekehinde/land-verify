263ea4b5fe7322aa5afb69b307b93b60aece2cf1
Kenny-Ibile <hussenagbaje@gmail.com>
2025-02-20 19:09:53 +0000

    adjusted clean up

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import '@/app/ui/global.css';
-import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import KeyFeatures from './components/features/keyfeatures';
 import Hero from './components/hero/hero';
