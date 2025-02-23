2e9bdf1db54c128703df413fb815b6bc2e18d744
Kenny-Ibile <hussenagbaje@gmail.com>
2025-02-23 17:07:16 +0000

    modified:   app/signup/page.tsx modified:   public/steps-image.png

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,9 +2,11 @@
 
 import React, { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import Link from 'next/link';
 
 export default function SignupForm() {
   const [form, setForm] = useState({ firstName: "", lastName: "", email: "" });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -12,6 +14,7 @@ export default function SignupForm() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
 
     const response = await fetch("/api/signup", {
       method: "POST",
@@ -20,6 +23,7 @@ export default function SignupForm() {
     });
 
     const data = await response.json();
+    setLoading(false);
     
     if (response.ok) {
       toast.success(data.message);
@@ -32,7 +36,9 @@ export default function SignupForm() {
   return (
     <div className="flex items-center justify-center min-h-screen p-4 sm:p-6 lg:p-8 bg-gray-100">
       <div className="max-w-xl mx-auto p-6 sm:p-8 lg:p-12 border border-gray-300 rounded-lg shadow-lg w-full bg-white">
-        <img src="/LandVerify-logo.png" alt="LandVerify Logo" className="mb-6 sm:mb-8 lg:mb-4 w-32 h-12 sm:w-40 sm:h-14 lg:w-48 lg:h-16 mx-auto"/>
+        <Link href="/">
+          <img src="/LandVerify-logo.png" alt="LandVerify Logo" className="mb-6 sm:mb-8 lg:mb-4 w-32 h-12 sm:w-40 sm:h-14 lg:w-48 lg:h-16 mx-auto"/>
+        </Link>
         <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-center mb-4 sm:mb-6 lg:mb-8">Create Account</h1>
         <form onSubmit={handleSubmit} className="space-y-4 p-4 rounded">
           <input
@@ -62,10 +68,13 @@ export default function SignupForm() {
             onChange={handleChange}
             className="block w-full p-2 border rounded hover:border-gray-500 focus:border-gray-500"
           />
-          <button type="submit" className="w-full py-2 bg-[#479101] text-white font-semibold rounded-md hover:bg-[#3a7a01]">
-            Create Account
+          <button type="submit" className="w-full py-2 bg-[#479101] text-white font-semibold rounded-md hover:bg-[#3a7a01] cursor-pointer">
+            {loading ? 'Creating Account...' : 'Create Account'}
           </button>
           <Toaster />
+          <div>
+            <p className="text-center mt-4">Already have an account? <a href="/login" className="text-[#479101] hover:underline">Login</a></p>
+          </div>
         </form>
       </div>
     </div>
