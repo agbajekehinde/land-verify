076cabd994cbe9ee55cddf0c68e09cf450cc3e70
Kenny-Ibile <hussenagbaje@gmail.com>
2025-02-24 20:05:34 +0000

    app/components/header/header.tsx

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,7 +7,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 flex items-center justify-between p-4 bg-white shadow-sm z-50">
       <div className="flex items-center">
-        <Link href="/signin">
+        <Link href="/">
           <Image
             src="/LandVerify-logo.png"
             alt="logo"
@@ -21,7 +20,7 @@ export default function Header() {
       <nav className="flex items-center gap-4">
         <Link href="/about" className="text-gray-800 hover:text-green-500 mx-2">About</Link>
         <Link href="/contact" className="text-gray-800 hover:text-green-500 mx-2">Contact</Link>
-        <Link href="/login" className="text-gray-800 hover:text-green-500 mx-2">Login</Link>
+        <Link href="/signin" className="text-gray-800 hover:text-green-500 mx-2">Login</Link>
       </nav>
     </header>
   );
