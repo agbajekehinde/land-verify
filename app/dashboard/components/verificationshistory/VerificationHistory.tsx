02efdcb988253f7b65d4cbb29081bacb3a665356
Kenny-Ibile <hussenagbaje@gmail.com>
2025-03-11 17:36:36 +0000

    modified:   app/dashboard/components/verificationshistory/VerificationHistory.tsx

--- app/dashboard/components/verificationshistory/VerificationHistory.tsx
+++ app/dashboard/components/verificationshistory/VerificationHistory.tsx
@@ -178,7 +178,7 @@ const VerificationHistory: React.FC = () => {
 
       {/* Modal for viewing verification details */}
       {selectedVerification && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+          <div className="fixed inset-0 bg-gray bg-opacity-10 backdrop-blur-md flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative max-h-[90vh] overflow-y-auto">
             <button
               className="absolute top-2 right-2 text-gray-600 hover:text-gray-800 cursor-pointer p-2 rounded-full hover:bg-gray-200 transition"
