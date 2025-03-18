05fbf12c7ccce5142634efb88710be71bb4bd43d
Kenny-Ibile <hussenagbaje@gmail.com>
2025-03-18 15:00:02 +0000

    modified:   app/dashboard/components/newverification/NewVerification.tsx

--- app/dashboard/components/newverification/NewVerification.tsx
+++ app/dashboard/components/newverification/NewVerification.tsx
@@ -569,7 +569,7 @@ export default function NewVerification({ isOpen, setIsOpen }: NewVerificationPr
                 <div className="w-full max-w-xs">
                   {(name && email) ? (
                     <PaystackButton
-                      text="Make Payment"
+                      text="Make Payment & Submit Verification"
                       className="w-full py-2 bg-green-500 text-white rounded hover:bg-green-600 flex items-center justify-center"
                       email={email}
                       amount={selectedPaymentPlan.amount}
