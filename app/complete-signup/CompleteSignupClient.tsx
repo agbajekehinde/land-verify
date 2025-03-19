57834782ee852ec5e1d9acb72ad71bb47af3f56b
Kenny-Ibile <hussenagbaje@gmail.com>
2025-03-19 15:44:51 +0000

    modified:   app/complete-signup/CompleteSignupClient.tsx

--- app/complete-signup/CompleteSignupClient.tsx
+++ app/complete-signup/CompleteSignupClient.tsx
@@ -199,30 +199,32 @@ export default function CompleteSignup() {
           />
 
           {/* Password field with toggle visibility button */}
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              placeholder="Password"
-              required
-              value={form.password}
-              onChange={handleChange}
-              className={`block w-full p-3 border rounded focus:border-gray-500 focus:outline-none pr-10 ${
-                form.password
-                  ? Object.values(passwordStrength).every(Boolean)
-                    ? "border-green-500"
-                    : "border-yellow-500"
-                  : ""
-              }`}
-            />
-            <button
-              type="button"
-              onClick={togglePasswordVisibility}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
-              aria-label={showPassword ? "Hide password" : "Show password"}
-            >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-            </button>
+          <div>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                required
+                value={form.password}
+                onChange={handleChange}
+                className={`block w-full p-3 border rounded focus:border-gray-500 focus:outline-none pr-10 ${
+                  form.password
+                    ? Object.values(passwordStrength).every(Boolean)
+                      ? "border-green-500"
+                      : "border-yellow-500"
+                    : ""
+                }`}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                aria-label={showPassword ? "Hide password" : "Show password"}
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
 
             {form.password && (
               <div className="mt-2 text-sm">
