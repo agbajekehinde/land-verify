fdf7acaffc37d0c78581b1928d84bcddbd32d8a5
Kenny-Ibile <hussenagbaje@gmail.com>
2025-04-02 14:16:09 +0100

    modified:   app/components/hero/hero.tsx

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -14,8 +14,7 @@ export default function Hero() {
           <h1 className="flex text-4xl text-gray-900 md:text-4xl md:leading-bold">
             <strong>Secure Your Land/Property Investment with Confidence</strong>
           </h1>
-          <p>Don’t fall victim to land/property fraud! Our expert team of licensed town planners, surveyors, and legal advisers will verify your property and provide expert advice. Get peace of mind before you buy—start your verification today! </p>
-          <div className="flex flex-col gap-4 md:flex-row w-full">
+          <p>Don’t fall victim to land/property fraud! Our licensed town planners, surveyors, and legal experts verify properties and provide expert advice within 48 hours. Ensure peace of mind before buying—start your verification today!</p>          <div className="flex flex-col gap-4 md:flex-row w-full">
             <Link
               href="/signup"
               className="flex w-full md:w-auto items-center justify-center gap-5 rounded-lg bg-[#3a7a01] px-6 py-4 md:py-3 text-sm font-medium text-white transition-colors hover:bg-[#3a7a01] md:text-base"
