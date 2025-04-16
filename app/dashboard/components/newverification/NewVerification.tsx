6ffdbadae12557c3faf7d2db4e85eda7dc5bbb64
Kenny-Ibile <hussenagbaje@gmail.com>
2025-04-16 20:10:14 +0100

    modified:   app/dashboard/components/newverification/NewVerification.tsx

--- app/dashboard/components/newverification/NewVerification.tsx
+++ app/dashboard/components/newverification/NewVerification.tsx
@@ -519,7 +519,7 @@ export default function NewVerification({ isOpen, setIsOpen }: NewVerificationPr
 
             <div className="grid grid-cols-2 gap-4">
               <div>
-                <label htmlFor="latitude" className="block text-sm font-medium text-gray-700">Latitude</label>
+                <label htmlFor="latitude" className="block text-sm font-medium text-gray-700">Eastings</label>
                 <input
                   id="latitude"
                   type="text"
@@ -532,7 +532,7 @@ export default function NewVerification({ isOpen, setIsOpen }: NewVerificationPr
               </div>
 
               <div>
-                <label htmlFor="longitude" className="block text-sm font-medium text-gray-700">Longitude</label>
+                <label htmlFor="longitude" className="block text-sm font-medium text-gray-700">Northings</label>
                 <input
                   id="longitude"
                   type="text"
