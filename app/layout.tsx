2bd3cccf972485ff33657026a88c441712db38a8
Kenny-Ibile <hussenagbaje@gmail.com>
2025-04-20 07:44:10 +0100

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'LandVerify',
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}
       <SpeedInsights /> 
+      <Analytics />
       </body>
     </html>
   );
