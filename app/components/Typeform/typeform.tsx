03c8ee78d6285a4a70e8e57197e5cb41a358dd55
Kenny-Ibile <hussenagbaje@gmail.com>
2025-06-19 12:31:00 +0100

    modified:   app/components/Typeform/typeform.tsx

--- app/components/Typeform/typeform.tsx
+++ app/components/Typeform/typeform.tsx
@@ -7,9 +7,8 @@ const Typeform = ()=>{
   return (
     <>
       <div data-tf-live="01JPYPF0K6KHNEDVBTQRZEX1JX"></div>
-      <Script src="https://embed.typeform.com/next/embed.js" strategy="afterInteractive" />
+      <img src='whatsapp.png' alt='WhatsApp' className='fixed bottom-4 right-4 w-42 h-14 z-50 cursor-pointer' onClick={() => window.open('https://wa.me/message/2M46JKYVDQRVM1', '_blank')} />
     </>
   );
 }
-
 export default Typeform;
