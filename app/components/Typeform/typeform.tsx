9ed9761fce7f738b3cf51c5884df2ed6c58dc13a
Kenny-Ibile <hussenagbaje@gmail.com>
2025-06-19 15:51:02 +0100

    modified:   app/components/Typeform/typeform.tsx

--- app/components/Typeform/typeform.tsx
+++ app/components/Typeform/typeform.tsx
@@ -6,7 +6,7 @@ const Typeform = ()=>{
   return (
     <>
       <div data-tf-live="01JPYPF0K6KHNEDVBTQRZEX1JX"></div>
-      <img src='whatsapp.png' alt='WhatsApp' className='fixed bottom-4 right-4 w-42 h-14 z-50 cursor-pointer' onClick={() => window.open('https://wa.me/message/2M46JKYVDQRVM1', '_blank')} />
+      <img src='whatsapp.png' alt='WhatsApp' className='fixed bottom-4 right-4 w-34 h-11 z-50 cursor-pointer' onClick={() => window.open('https://wa.me/message/2M46JKYVDQRVM1', '_blank')} />
     </>
   );
 }
