1f98a8c4242fd16fdfee975df6b38e54d03ed145
Kenny-Ibile <hussenagbaje@gmail.com>
2025-06-29 09:21:24 +0100

    modified:   app/components/features/keyfeatures.tsx

--- app/components/features/keyfeatures.tsx
+++ app/components/features/keyfeatures.tsx
@@ -12,12 +12,12 @@ export default function KeyFeatures() {
           <div className="flex flex-1 flex-col items-center text-center p-6 bg-white rounded-lg shadow-md h-64">
             <ShieldCheckIcon className="w-12 h-12 text-[#479101]" />
             <h3 className="mt-4 text-xl font-semibold text-gray-800">Protects Investment</h3>
-            <p className="mt-2 text-gray-600">With intelligent GIS technology & expert verification report, we tell you if its safe, risky or red flag investment before buying.</p>
+            <p className="mt-2 text-gray-600">Intelligent GIS technology and expert reports tell you if your property is safe, risky, or a red-flag investment-before you buy.</p>
           </div>
           <div className="flex flex-1 flex-col items-center text-center p-6 bg-white rounded-lg shadow-md h-64">
             <UserGroupIcon className="w-12 h-12 text-[#479101]" />
             <h3 className="mt-4 text-xl font-semibold text-gray-800">Expert Verification</h3>
-            <p className="mt-2 text-gray-600">A dedicated team of licensed town planners, surveyors, and legal professionals with extensive experience.</p>
+            <p className="mt-2 text-gray-600">Our in-house team of licensed town planners, surveyors, and legal professionals delivers thorough, expert-backed verification.</p>
           </div>
           <div className="flex flex-1 flex-col items-center text-center p-6 bg-white rounded-lg shadow-md h-64">
             <ClockIcon className="w-12 h-12 text-[#479101]" />
